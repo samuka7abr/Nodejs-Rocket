6576eb9acd534ab8e5ec1a867998f4ed0babf5e5
samuka7abr <samuelabrao2006@gmail.com>
2025-04-02 16:07:16 -0300

    chore: tratando requisições

--- src/database.js
+++ src/database.js
@@ -1,19 +1,39 @@
-export class Database{
-    database = {}
+import fs from 'node:fs/promises'
 
-    select(table){
-        const data = this.database[table] ?? []
+const databasePath = new URL('../db.json', import.meta.url)
 
-        return data
+export class Database {
+  #database = {}
+
+  constructor() {
+    fs.readFile(databasePath, 'utf8')
+      .then(data => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => {
+        this.#persist()
+      })
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
+
+  select(table) {
+    const data = this.#database[table] ?? []
+
+    return data
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data)
+    } else {
+      this.#database[table] = [data]
     }
 
-    insert(table, data){
-        if(Array.isArray(this.database[table])){
-            this.database[table].push(data)
-        }else{
-            this.database[table] = [data]
-        }
+    this.#persist()
 
-        return data;
-    }
+    return data
+  }
 }
