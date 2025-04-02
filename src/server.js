f3c54f1755f8f7369385c9c5b2d34c8f63f2a286
samuka7abr <samuelabrao2006@gmail.com>
2025-04-02 15:22:40 -0300

    chore: tratando requisições

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
-import http from 'node:http'
+import http from "node:http";
+import { json } from "node:stream/consumers";
 
 // - Criar usuários
 // - Listagem usuários
@@ -26,42 +27,30 @@ import http from 'node:http'
 
 // HTTP Status Code
 
-const users = []
+const users = [];
 
 const server = http.createServer(async (req, res) => {
-  const { method, url } = req
-
-  const buffers = []
-
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    req.body = null
-  }
-
-  if (method === 'GET' && url === '/users') {
+  const { method, url } = req;
+  json;
+  if (method === "GET" && url === "/users") {
     return res
-      .setHeader('Content-type', 'application/json')
-      .end(JSON.stringify(users))
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = req.body
+  if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
 
     users.push({
       id: 1,
       name,
       email,
-    })
+    });
 
-    return res.writeHead(201).end()
+    return res.writeHead(201).end();
   }
 
-  return res.writeHead(404).end()
-})
+  return res.writeHead(404).end();
+});
 
-server.listen(3333)
+server.listen(3333);
