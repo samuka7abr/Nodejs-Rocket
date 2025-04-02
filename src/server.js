b11a158e03b8f3227a42162786c4137a8c45c94f
samuka7abr <samuelabrao2006@gmail.com>
2025-04-02 16:02:33 -0300

    chore: tratando requisições

--- src/server.js
+++ src/server.js
@@ -1,38 +1,16 @@
 import http from "node:http";
 import { json } from "./middlewares/json.js";
+import { Database } from "./database.js";
 
-// - Criar usuários
-// - Listagem usuários
-// - Edição de usuários
-// - Remoção de usuários
-
-// - HTTP
-//   - Método HTTP
-//   - URL
-
-// GET, POST, PUT, PATCH, DELETE
-
-// GET => Buscar um recurso do back-end
-// POST => Criar um recurso no back-end
-// PUT => Atualizar um recurso no back-end
-// PATCH => Atualizar uma informação específica de um recurso no back-end
-// DELETE => Deletar um recurso do back-end
-
-// GET /users => Buscando usuários no banc-end
-// POST /users => Criar um usuário no back-end
-
-// Stateful - Stateless
-
-// Cabeçalhos (Requisição/resposta) => Metadados
-
-// HTTP Status Code
-
+const database = new Database();
 const users = [];
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
   await json(req,res)
   if (method === "GET" && url === "/users") {
+    const users = database.select('users')
+
     return res
       .setHeader("Content-type", "application/json")
       .end(JSON.stringify(users));
@@ -41,11 +19,13 @@ const server = http.createServer(async (req, res) => {
   if (method === "POST" && url === "/users") {
     const { name, email } = req.body;
 
-    users.push({
+    const user = ({
       id: 1,
       name,
       email,
     });
+
+    database.insert('users', user)
 
     return res.writeHead(201).end();
   }
