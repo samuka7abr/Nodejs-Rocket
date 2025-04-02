8960631ad4b4d10c9910978bf7fee05b21b1743c
samuka7abr <samuelabrao2006@gmail.com>
2025-04-02 16:22:12 -0300

    chore: tratando requisições

--- src/server.js
+++ src/server.js
@@ -1,36 +1,36 @@
-import http from "node:http";
-import { json } from "./middlewares/json.js";
-import { Database } from "./database.js";
+import http from 'node:http'
+import { randomUUID } from 'node:crypto'
+import { Database } from './database.js'
+import { json } from './middlewares/json.js'
 
-const database = new Database();
-const users = [];
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
-  const { method, url } = req;
-  await json(req,res)
-  if (method === "GET" && url === "/users") {
+  const { method, url } = req
+
+  await json(req, res)
+
+  if (method === 'GET' && url === '/users') {
     const users = database.select('users')
 
-    return res
-      .setHeader("Content-type", "application/json")
-      .end(JSON.stringify(users));
+    return res.end(JSON.stringify(users))
   }
 
-  if (method === "POST" && url === "/users") {
-    const { name, email } = req.body;
+  if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body
 
-    const user = ({
-      id: 1,
+    const user = {
+      id: randomUUID(),
       name,
       email,
-    });
+    }
 
     database.insert('users', user)
 
-    return res.writeHead(201).end();
+    return res.writeHead(201).end()
   }
 
-  return res.writeHead(404).end();
-});
+  return res.writeHead(404).end()
+})
 
-server.listen(3333);
+server.listen(3333)
