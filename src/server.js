c23b40672e5889f573bb045e34eb0c870a63e997
samuka7abr <samuelabrao2006@gmail.com>
2025-04-02 15:12:37 -0300

    chore: tratando requisições

--- src/server.js
+++ src/server.js
@@ -1,28 +1,67 @@
-import http from 'http';
+import http from 'node:http'
+
+// - Criar usuários
+// - Listagem usuários
+// - Edição de usuários
+// - Remoção de usuários
+
+// - HTTP
+//   - Método HTTP
+//   - URL
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET => Buscar um recurso do back-end
+// POST => Criar um recurso no back-end
+// PUT => Atualizar um recurso no back-end
+// PATCH => Atualizar uma informação específica de um recurso no back-end
+// DELETE => Deletar um recurso do back-end
+
+// GET /users => Buscando usuários no banc-end
+// POST /users => Criar um usuário no back-end
+
+// Stateful - Stateless
+
+// Cabeçalhos (Requisição/resposta) => Metadados
+
+// HTTP Status Code
 
 const users = []
 
-const server = http.createServer((req, res) =>{
-    const { method, url } = req;
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
 
-    if(method === 'GET' && url === '/users'){
-        return res
-        .setHeader('Content-type', 'application/json')
-        .end(JSON.stringify(users))
-    }
+  const buffers = []
 
-    if(method === 'POST' && url === '/users'){
-        users.push({
-            id: 1,
-            name: 'samuel',
-            email: 'samuel@example.com'
-        })
-        return res.end('criação de usuário');
-    }
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
 
-    return res.end('hello world')
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    req.body = null
+  }
+
+  if (method === 'GET' && url === '/users') {
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body
+
+    users.push({
+      id: 1,
+      name,
+      email,
+    })
+
+    return res.writeHead(201).end()
+  }
+
+  return res.writeHead(404).end()
 })
 
-const PORT = 3333;
-server.listen(PORT);
-console.log(`Server is running on port 3333`)
+server.listen(3333)
