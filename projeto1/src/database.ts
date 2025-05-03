4102b11b977403c753349f1fbc7392f25d7e44d8
samuka7abr <samuelabrao2006@gmail.com>
2025-05-02 21:08:55 -0300

    variáveis de ambiente

--- projeto1/src/database.ts
+++ projeto1/src/database.ts
@@ -1,8 +1,21 @@
+import path from 'node:path';
 import knex from 'knex';
+import { env } from './env/index';
 
-export const database = knex({
-  client: 'sqlite3',
-  connection: {
-    filename: './tmp/app.db'
-  },
-});
+if(process.env.DATABASE_URL){
+	throw new Error('database not provided');
+}
+
+export const config: Parameters<typeof knex>[0] = {
+	client: 'sqlite3',
+	connection: {
+		filename: env.DATABASE_URL,
+	},
+	useNullAsDefault: true,
+	migrations: {
+		extension: 'ts',
+		directory: path.resolve('db', 'migrations')
+	}
+};
+
+export const database = knex(config);
--- projeto1/src/server.ts
+++ projeto1/src/server.ts
@@ -1,13 +1,20 @@
 import fastify from 'fastify';
 import { database } from './database'; 
+import crypto from 'node:crypto';	
+import { env } from './env';
 
 const app = fastify();
 
 app.get('/hello', async () => {
-  const tables = await database('sqlite_schema').select('*');
-  return tables;
+	const transactions = await database('transactions').insert({
+		id: crypto.randomUUID(),
+		title: 'transação de teste',
+		amount: 1000
+	}).returning('*');
+
+	return transactions;
 });
 
-app.listen({ port: 3333 }).then(() => {
-  console.log('HTTP server running!');
+app.listen({ port: env.PORT }).then(() => {
+	console.log('HTTP server running!');
 });
